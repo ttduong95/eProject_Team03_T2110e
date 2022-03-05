c7be3d163f667dde933b79f6b4992973caca175c
ttduong95 <92871903+ttduong95@users.noreply.github.com>
2022-03-06 03:07:42 +0700

    up code

--- index.js
+++ index.js
@@ -7,6 +7,26 @@ app.listen(port,function (err) {
 app.use(express.static("public"));// cho quyền truy cập toàn bộ các file tĩnh bên trong thư mục public
 app.use(express.static("imge"));
 app.set("view engine","ejs");
+var mssql = require("mssql");
+var config = {
+    server: "118.70.125.210",
+    user:"sa",
+    password:"z@GH7ytQ",
+    database:"QuangHoa",
+    stream: false,
+    port:1433,
+    options: {
+        trustedConnection: true,
+        encrypt: true,
+        enableArithAbort: true,
+        trustServerCertificate: true,
+    },
+}
+mssql.connect(config,function (err){
+    if(err) console.log(err);
+    else console.log("connected database..");
+});
+var sql = new mssql.Request();
 app.get("/",function (req, res) {
     res.render("homepage");
 })
