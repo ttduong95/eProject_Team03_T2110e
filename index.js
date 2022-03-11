f5dee999bd23249282ce88649bb360541ced7a0e
ttduong95 <92871903+ttduong95@users.noreply.github.com>
2022-03-11 20:13:02 +0700

    up code ddd

--- index.js
+++ index.js
@@ -6,44 +6,28 @@ app.listen(port,function (err) {
 })
 app.use(express.static("public"));
 app.set("view engine","ejs");
-var mssql = require("mssql/msnodesqlv8");
-var config = {
-    port: 1433,
-    server: "118.70.125.210",
-    user: "sa",
-    password: "z@GH7ytQ",
-    database: "Nhom03",
-    driver: "msnodesqlv8",
-    stream: false,
-    options:{
 
-    },
-
-}
-mssql.connect(config,function (err){
-    if(err) console.log(err);
-    else console.log("connected database....")
-});
-
-var sql = new mssql.Request();
 app.get("/",function (req, res) {
     res.render("homepage");
 })
 app.get("/contacts",function (req, res) {
     res.render("contacts");
 })
-app.get("/shop-tung",function (req, res) {
-    res.render("shop-tung");
-})
-
-app.get("/sp",function (req,res){
+app.get("/shop",function (req, res) {
     var param =req.query.NameProduct;
     var sql_txt ="select * from T2110E_Nhom3_Product where NameProduct like '%"+param+"%';";
     sql.query(sql_txt,function (err,rs){
         if(err) res.send("Errors..");
-        else res.render("shop",{
+        else res.render("shop-tung",{
             products:rs.recordset
         });
     });
+
+})
+app.get("/detail",function (req, res) {
+    res.render("product");
+})
+
+app.get("/sp",function (req,res){
 
 })
