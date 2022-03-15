7967c0d49042b2dfb2c8bc23adcf4fed44bf1ca4
ttduong95 <92871903+ttduong95@users.noreply.github.com>
2022-03-15 22:38:43 +0700

    up code ddd

--- index.js
+++ index.js
@@ -6,25 +6,45 @@ app.listen(port,function (err) {
 })
 app.use(express.static("public"));
 app.set("view engine","ejs");
-var mssql = require("mssql/msnodesqlv8");
-var config = {
-    port: 1433,
-    server: "118.70.125.210",
-    user: "sa",
-    password: "z@GH7ytQ",
-    database: "Nhom03",
-    stream: false,
-    options:{
+var mysql=require('mysql');
+var db_config = {
+    host:'remotemysql.com',
+    user:'kA3SOoSF6H',
+    password:'Z6dM0YmYuI',
+    database:'kA3SOoSF6H',
+    port:3306,
+    stream:false,
+    options: {
 
-    },
+    }
+};
+var conn;
+function handleDisconnect() {
+    conn = mysql.createConnection(db_config); // Recreate the connection, since
+    // the old one cannot be reused.
+    // the old one cannot be reused.
 
+    conn.connect(function(err) {              // The server is either down
+        if(err) {                                     // or restarting (takes a while sometimes).
+            console.log('error when connecting to db:', err);
+            setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+        }// to avoid a hot loop, and to allow our node script to
+        else console.log('Connected to database')// to avoid a hot loop, and to allow our node script to
+    });                                     // process asynchronous requests in the meantime.
+                                            // If you're also serving http, display a 503 error.
+    conn.on('error', function(err) {
+        console.log('db error', err);
+        if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+            handleDisconnect();                         // lost due to either server restart, or a
+        } else {                                      // connnection idle timeout (the wait_timeout
+            throw err;                                  // server variable configures this)
+        }
+    });
 }
-mssql.connect(config,function (err){
-    if(err) console.log(err);
-    else console.log("connected database....")
-});
 
-var sql = new mssql.Request();
+handleDisconnect();
+//end connect to database
+
 app.get("/",function (req, res) {
     res.render("homepage");
 })
@@ -32,33 +52,30 @@ app.get("/contacts",function (req, res) {
     res.render("contacts");
 })
 app.get("/shop",function (req, res) {
-    var param =req.query.AllProduct;
-    var sql_name ="select * from T2110E_Nhom3_Product where NameProduct like '%"+param+"%';"+
-    "select * from T2110E_Nhom3_Product order by NameProduct desc;"+
-    "select * from T2110E_Nhom3_Product where Price like '%"+param+"%';" +
-    "select * from T2110E_Nhom3_Product where Color1 like '%"+param+"%';";
+    var param = req.query.AllProduct;
+        var sql_name = "select * from T2110E_Nhom3_Product where NameProduct like '%" + param + "%';";
+        conn.query(sql_name, function (err, rs) {
+            if (err) console.log(err);
+            else {
+                res.render("shop-tung", {
+                    products:rs,
+                });
+            }
+        });
 
-    sql.query(sql_name,function (err,rs){
-        if(err) res.send("Errors..");
-        else{
-           res.render("shop-tung",{
-           products:rs.recordsets[0],
-               size:rs.recordsets[2],
-               color:rs.recordsets[3],
-           });
-        }
-    });
-})
+    })
+
 app.get("/product",function (req, res) {
     var param = req.query.id;
     var sql_txt ="select * from T2110E_Nhom3_Product where ID = "+param+";";
-    sql.query(sql_txt,function (err, rs) {
-        if(err) res.send("Errors..");
-        else if(rs.recordset.length ==0){
+    conn.query(sql_txt,function (err, rs) {
+        if(err) console.log(err);
+
+        else if(rs.length ==0){
             res.send("Không tìm thấy sản phẩm nào hết");
         }else{
             res.render("product",{
-                product:rs.recordset[0]
+                product:rs[0]
             })
         }
     })
@@ -66,7 +83,7 @@ app.get("/product",function (req, res) {
 app.get("/color",function (req, res) {
     var param = req.query.color1;
     var sql_txt = "select * from T2110E_Nhom3_Product where Color1 like '%"+param+"%';";
-    sql.query(sql_txt,function (err,rs){
+    conn.query(sql_txt,function (err,rs){
         if(err) res.send("Errors..");
         else res.render("color",{
             colors:rs.recordset,
